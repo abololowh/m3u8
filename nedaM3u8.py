6e4ae568bc0779bc31ba05f1a0fc5d28498d1997
abololowh <d7.0@hotmail.com>
2024-05-20 01:29:13 +0300

    Update nedaM3u8.py

--- nedaM3u8.py
+++ nedaM3u8.py
@@ -19,28 +19,4 @@ def get_m3u8_url():
             print("An error occurred during the download:")
             print(str(e))
 
-def get_value_from_url(url):
-    try:
-        # إرسال طلب GET لفتح الرابط
-        response = requests.get(url)
-        
-        # التأكد من نجاح الطلب
-        if response.status_code == 200:
-            # إرجاع المحتوى المسترجع
-            return response.text
-        else:
-            print("Failed to retrieve content. Status code:", response.status_code)
-            return None
-    except Exception as e:
-        print("An error occurred:", e)
-        return None
-
-# تحديد الرابط الذي تريد فتحه
-url = get_m3u8_url()
-# استخدام الدالة لفتح الرابط واسترداد المحتوى
-content = get_value_from_url(url)
-if content:
-    print("Content retrieved successfully:")
-    print(content)
-else:
-    print("Failed to retrieve content from the URL.")
+ get_m3u8_url()
